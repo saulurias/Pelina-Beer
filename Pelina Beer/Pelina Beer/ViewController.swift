8b50134d1b2018f72569ed8b98039974f55ab653
Saul <saulurias08@gmail.com>
2020-03-02 08:16:17 -0700

    Project Created

---
+++ Pelina Beer/Pelina Beer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pelina Beer
+//
+//  Created by SaulUrias on 02/03/20.
+//  Copyright © 2020 saulurias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
