92c95ab57060128d923879643385292cf5761c36
Saul <saulurias08@gmail.com>
2020-03-02 08:30:20 -0700

    Configured SwiftLint With CocoaPods

---
+++ Pelina Beer/Pods/Target Support Files/Pods-Pelina Beer-Pelina BeerUITests/Pods-Pelina Beer-Pelina BeerUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Pelina_Beer_Pelina_BeerUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Pelina_Beer_Pelina_BeerUITests
+@end
---
+++ Pelina Beer/Pods/Target Support Files/Pods-Pelina BeerTests/Pods-Pelina BeerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Pelina_BeerTests : NSObject
+@end
+@implementation PodsDummy_Pods_Pelina_BeerTests
+@end
